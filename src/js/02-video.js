c18eb27ccbdee9001334668d758cb9c784ca4bd8
Anka <103377387+ann153476@users.noreply.github.com>
2023-01-12 21:17:09 +0200

    new

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,33 @@
-//The data-main attribute is a special attribute that require.js will check to start script loading.
-
+import vimeo from "@vimeo/player";
+// отето vimeo 
+import Throttle from "lodash.throttle";
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const player = new vimeo(iframe);
+//отут вставляэться |^|
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+    // player.on('timeupdate', function(event) {
+    //     const a =event.seconds;
+    //     localStorage.setItem("videoplayer-current-time", a);
+    // });
+    player.on('timeupdate', Throttle( (event)=> {
+        const a =event.seconds;
+        console.log(a);
+        localStorage.setItem("videoplayer-current-time", a);
+    },1000));
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+   const getLastTime =localStorage.getItem("videoplayer-current-time");
+   //console.log( getLastTime,"этo последнее время");
+   player.setCurrentTime(getLastTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+    
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import Throttle from "lodash.throttle";
+
+const formEl = document.querySelector('form');
+
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+getobj =localStorage.getItem("feedback-form-state") || {};
+
+function myInput (event){
+    event.preventDefault();
+    const obj = {
+        email:email.value,
+        message:message.value
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(obj));
+}
+
+formEl.addEventListener("input", Throttle(myInput , 500));
+
+formEl.addEventListener(`submit`, event => {
+    event.preventDefault();
+    if(email.value || message.value === true){
+        formEl.reset();
+    }else{
+        console.log("заповни поля");
+    }
+    
+});
+
+console.log(getobj);
+
 
