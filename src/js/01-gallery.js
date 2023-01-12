eafe9bceb92753289ab440517a83dbba88db93fd
Anka <103377387+ann153476@users.noreply.github.com>
2023-01-12 11:46:08 +0200

    new

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector(".gallery");
+
+
+const markup = galleryItems
+.map((galleryItem) =>
+ `<a class="gallery__item" href="${galleryItem.original}">
+ <img class="gallery__image" 
+ src="${galleryItem.preview}" 
+ data-source="${galleryItem.description}"
+ alt="${galleryItem.description}" />
+</a>`).join("");
+
+  gallery.insertAdjacentHTML("beforeend", markup);
+
+  let galleryModal = new SimpleLightbox('.gallery a',
+  {captionsData: "alt",
+  captionDelay: 250,
+  captionType: "alt",}
+  );
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+//The data-main attribute is a special attribute that require.js will check to start script loading.
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
